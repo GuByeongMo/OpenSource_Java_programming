c398e3783c4532806892384df2c65ce1407d1552
GuByeongMo <118723152+GuByeongMo@users.noreply.github.com>
2023-11-30 15:39:11 +0900

    Add files via upload
    자바 100문제22

---
+++ ex100_89.java
@@ -0,0 +1,21 @@
+package java_ex100;
+
+public class ex100_89 {
+
+	public static void main(String[] args) {
+		// 1, 4
+		// 2, 3
+		// 3, 2
+		// 4, 1
+		int sum = 12;
+		for(int i=1; i<=6; i++) {
+			for(int j=1; j<=6; j++) {
+				if(i+j == sum) {
+					System.out.println(i+","+j);
+				}
+			}
+		}
+		
+	}
+
+}
